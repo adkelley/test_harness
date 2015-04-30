c3dd2a96bdd900493104d7161a5f56f0264da16c
Ilias Tsangaris <iliastsangaris@gmail.com>
2015-04-30 15:37:43 -0700

    Refactor
    General refactor: nested your functions in to an AK (Alex Kelley) object, renamed some functions. Btw this is great, love it!

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-// Use to test my functions
-function assert (value, desc) {
-  var li = document.createElement ("li");
-  li.className = value ? "pass" : "fail";
-  li.appendChild (document.createTextNode (desc));
-  document.getElementById ("results").appendChild (li);
-}
-
-// Write a function to find the maximum number in an array.
-var getMax = function (arr) {
-  var max = arr[0];
-  for (var i=1; i<arr.length; i++) {
-    (max < arr[i]) ? max = arr[i] : max;
+//AK Library
+var AK = {
+  render: function(value, desc) {
+    var li = document.createElement ("li");
+    li.className = value ? "pass" : "fail";
+    li.appendChild (document.createTextNode (desc));
+    document.getElementById ("results").appendChild (li);
+  },
+  test: function(testFunction, input, expectedOutput) {
+    var passOrFail = testFunction(input) === expectedOutput;
+    var functionName = testFunction.name;
+    var message = functionName + (passOrFail ? "passed!" : "failed.");
+    this.render(passOrFail, message);
   }
-  return max;
 }
 
-var test = function() {
-  assert(true, "Testing Functions");
-  assert(getMax([0, 1, 2, 3, 4, 5]) === 5, "getMax() returns 5");
-
+//Example test functions, add yours here
+var testing = {
+  getMax: function(arr) {
+    var max = arr[0];
+    for (var i=1; i<arr.length; i++) {
+      (max < arr[i]) ? max = arr[i] : max;
+    }
+    return max;
+  }
 }
 
+//Run test functions here
+var testSuite = function() {
+  AK.test(testing.getMax, [1,2,3,4,5], 5);
+  AK.test(testing.getMax, [1,2,3,4,5], 4);
+}
